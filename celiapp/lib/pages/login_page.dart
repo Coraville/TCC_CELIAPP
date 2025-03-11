1ce2c0b861fd4086577d230200e03d640ef3300e
Laiz <laizptorres@hotmail.com>
2025-03-11 13:31:29 -0300

    login page 1.0

--- celiapp/lib/pages/login_page.dart
+++ celiapp/lib/pages/login_page.dart
@@ -8,51 +8,107 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  
   TextEditingController emailController = TextEditingController();
   TextEditingController senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: _page(),
-    );
+    return Scaffold(backgroundColor: Colors.white, body: _page());
   }
 
   Widget _page() {
     return Padding(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(40.0),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          _inputField("e-mail", emailController),
-          const SizedBox(height:50),
-          _inputField("senha", senhaController, isPassword: true),
-          const SizedBox(height: 50),
+            const Text(
+              "Celiapp",
+              style: TextStyle(
+                fontFamily: 'Krona One',
+                fontSize: 32,
+                color: Colors.deepOrangeAccent,
+              ),
+            ),
+            const SizedBox(height: 55),
+            _inputField("e-mail", emailController),
+            const SizedBox(height: 15),
+            _inputField("senha", senhaController, isPassword: true),
+            const SizedBox(height: 10),
+            _loginBtn(),
+            const SizedBox(height: 70),
+            _registerBtn(),
+            _forgotpass()
           ],
         ),
       ),
     );
   }
 
-  Widget _inputField(String hintText, TextEditingController controller, 
-    {isPassword = false}){
+
+  Widget _inputField(
+    String hintText,
+    TextEditingController controller, {
+    isPassword = false,
+  }) {
     var border = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10.0),
-      borderSide: const BorderSide(color: Colors.grey)
+      borderRadius: BorderRadius.circular(15.0),
+      borderSide: const BorderSide(color: Colors.grey),
     );
     return TextField(
-      style: const TextStyle(color: Colors.grey),
+      style: const TextStyle(fontFamily: 'RobotoFlex', color: Colors.black),
       controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
-        hintStyle: const TextStyle(color: Colors.grey),
+        hintStyle: const TextStyle(
+          fontFamily: 'RobotoFlex',
+          color: Colors.grey,
+        ),
         enabledBorder: border,
-        focusedBorder: border
+        focusedBorder: border,
+        isDense: true,
       ),
       obscureText: isPassword,
+    );
+  }
+
+  Widget _loginBtn(){
+    return TextButton(
+        onPressed: (){
+          debugPrint("email : ${emailController.text}" );
+          debugPrint("senha : ${senhaController.text}" );
+        },
+        child: const Text('login', style: TextStyle(
+            fontFamily: 'RobotoFlex',
+            color: Colors.deepOrangeAccent,
+          fontSize: 15
+        )
+      )
+    );
+  }
+
+  Widget _registerBtn(){
+    return TextButton(
+        onPressed: (){},
+        child: const Text('registre-se', style: TextStyle(
+            fontFamily: 'RobotoFlex',
+            color: Colors.deepOrangeAccent,
+            fontSize: 15
+        )
+        )
+    );
+  }
+
+  Widget _forgotpass(){
+    return TextButton(
+        onPressed: (){},
+        child: const Text('esqueceu a senha?', style: TextStyle(
+            fontFamily: 'RobotoFlex',
+            color: Colors.deepOrangeAccent,
+            fontSize: 15
+        )
+        )
     );
   }
 }
