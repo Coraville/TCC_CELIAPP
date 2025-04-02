c3d0a99192421106a7dba2c5a4a404dd40a179c9
Coraville <dumont2700@gmail.com>
2025-04-02 16:32:52 -0300

    Firebase adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,35 @@
+import 'package:celiapp/login_page.dart';
 import 'package:flutter/material.dart';
 import 'loading_screen.dart'; // Importando a página de carregamento
 import 'shopping_list_page.dart';
 import 'profile_page.dart';
 import 'map_page.dart';
 import 'recipes_page.dart';
-import 'info_page.dart';
-import 'login_page.dart'; // Certifique-se de ter a página de login criada
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const CeliApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
-class CeliApp extends StatelessWidget {
-  const CeliApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CeliApp',
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const LoadingScreen(), // Tela de carregamento
-        '/shopping_list': (context) => const ShoppingListPage(),
-        '/profile': (context) => const ProfilePage(),
-        '/map': (context) => const MapPage(),
-        '/recipes': (context) => const RecipesPage(),
-        '/info': (context) => const InfoPage(),
-        '/login': (context) => const LoginPage(),
-      },
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const LoginPage(),
     );
   }
 }
-
-
